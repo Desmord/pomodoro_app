a1bd9eac4cdcc54b41e0794987785f0a30525409
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-08-11 18:33:42 +0200

    Init Project:
    	- Init project;
    	- Delete unnessesery files;
    	- Install liblarys;
    	- Create reset, setting and global styles;

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './Components/App/App';
+
+import './GlobalStyles/Reset.scss';
+import './GlobalStyles/Settings.scss';
+import './GlobalStyles/Global.scss';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
