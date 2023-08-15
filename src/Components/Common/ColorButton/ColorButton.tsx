89936ef60b5ad325b6e2fde38f59cc50573a07c6
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-08-15 17:05:54 +0200

    Create ColorButton component

---
+++ src/Components/Common/ColorButton/ColorButton.tsx
@@ -0,0 +1,29 @@
+import { useContext } from 'react';
+import { AppContext } from '../../../Context/AppContext';
+import { COLOR_THEMES } from '../../../Utilities/Types';
+import { MdOutlineDone } from 'react-icons/md';
+
+import styles from './ColorButton.module.scss';
+
+/**
+ * <FontButton text={COLOR_THEMES.ORANGE} />
+ * 
+ * @param param0 string COLOR_THEMES
+ * @returns 
+ */
+const ColorButton = ({ color }: { color: string }) => {
+    const theme = useContext(AppContext);
+
+    return (
+        <div className={`
+            ${styles.container}
+            ${color === COLOR_THEMES.ORANGE ? styles[`color-orange`] :
+                color === COLOR_THEMES.AQUARMARINE ? styles[`color-aquarmarine`] :
+                    color === COLOR_THEMES.VIOLET ? styles[`color-violet`] : ``}
+        `}>
+            {theme.colorTheme === color ? <MdOutlineDone /> : ``}
+        </div>
+    )
+}
+
+export default ColorButton;
